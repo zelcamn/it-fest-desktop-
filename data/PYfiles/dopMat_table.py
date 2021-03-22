cd0ed24219ce22bd06b42cf16cc66cce6b7ae2fc
pavel-056 <pavel.semyonov.2605@yandex.ru>
2021-03-22 17:56:43 +0900

    efwe

--- data/PYfiles/dopMat_table.py
+++ data/PYfiles/dopMat_table.py
@@ -1,6 +1,5 @@
 from PyQt5.QtWidgets import QTableWidget
 
-
 class OptionalMaterialsTable:
     def __init__(self, objlist):
         self.table = objlist['table']
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5 import QtGui
+from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
 from data.PYfiles.dopMat_table import *
 from data.PYfiles.mh_table import *
 from data.PYfiles.stan_ui import *
+from data.PYfiles.calculate import *
+
+
+ManHourTableRes = 0
+BenchTableRes = 0
+OptMatRes = 0
+Result = 0
+tax = 5.66
 
 
 class Main(QMainWindow):
@@ -18,6 +25,10 @@ class Main(QMainWindow):
 
         ManHoursList = {'table': self.mh_table, 'addbtn': self.mh_add, 'delbtn': self.mh_del}
         MHTable = ManHoursTable(ManHoursList)
+
+        calclist = {'resbtn': self.count_bt, 'reslcd': self.Cost_LCD, 'optmattable': self.mat_table,
+                    'manhourtable': self.mh_table, 'benchtable': self.bench_list_table}
+        objCalculator = Calculator(calclist)
 
         BenchList = {'table': self.bench_list_table, 'addbtn': self.bench_add, 'delbtn': self.bench_del}
         BenchTable = BenchOperationsTable(BenchList)
